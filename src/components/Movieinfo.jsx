b5c24829b085066ffdbfb1f553d534b91e2db768
shadab7477 <techshadab04@gmail.com>
2024-05-26 22:09:30 +0530

    six

--- src/components/Movieinfo.jsx
+++ src/components/Movieinfo.jsx
@@ -6,12 +6,13 @@ import { useState,useEffect } from 'react'
 const Movieinfo = () => {
     const {id}=useParams()
 
-const [movie,setmovie]=useState(['']
+const [movie,setmovie]=useState([''])
+const [trailers,settrailer]=useState([''])
 
-)
-
+console.log(trailers);
 useEffect(() => {
     getdetail()
+    gettrailer()
 }, [''])
 
 
@@ -21,12 +22,21 @@ const getdetail=async()=>{
 setmovie(data.data)
 }
 
-console.log(movie);
 
-console.log(id);
-  return (<>
+const gettrailer=async()=>{
+    const datas=await axios.get(`https://api.themoviedb.org/3/movie/${id}/videos?api_key=4e44d9029b1270a757cddc766a1bcb63&language=en-US`)
+//    settrailer(datas);
+     if (datas.data.results.length > 0) {
+         settrailer(datas.data.results[0])
   
-  <div class="container8">
+         } else {
+           alert("Trailer not found. please search another movie")
+       }
+}
+
+return (<>
+  
+  <div class="container8 container ">
   <div class="background">
     <img alt="Title" src={`https://image.tmdb.org/t/p/original${movie && movie.backdrop_path}`} />
   </div>
@@ -58,6 +68,34 @@ console.log(id);
   </div>
 </div>
 
+{/* <div className="video-responsive w-100  py-2 ">
+    <iframe
+      width="100%"
+      height="550vh"
+      src={`https://www.youtube.com/embed/${trailers.key}?version=3&vq=hd720`}
+      frameBorder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowFullScreen
+      title="Embedded youtube"
+    />
+  </div> */}
+
+{/* <iframe width="560" height="315" src="https://www.youtube.com/embed/vlytpc9fxZM?si=zXRNOXJgclpTT8p-" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe> */}
+
+
+    
+<div className="video-responsive container py-2 ">
+    <iframe
+    id='responsiveyt'
+      width="100%"
+      height="500vh"
+      src={`https://www.youtube.com/embed/${trailers.key}?version=3&vq=hd720`}
+      frameBorder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowFullScreen
+      title="Embedded youtube"
+    />
+  </div>
   </>
   )
 }
